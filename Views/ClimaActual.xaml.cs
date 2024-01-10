6c9ec56048f8e7a0cec6bc07ef9bf33bb3a73bda
ramoscariel <103594586+ramoscariel@users.noreply.github.com>
2024-01-10 08:54:38 -0500

    Implementado la llamada de api

--- Views/ClimaActual.xaml.cs
+++ Views/ClimaActual.xaml.cs
@@ -1,3 +1,7 @@
+using Newtonsoft.Json;
+using Ramos_ApiPublica.Model;
+using System.Text.Json.Serialization;
+
 namespace Ramos_ApiPublica.Views;
 
 public partial class ClimaActual : ContentPage
@@ -7,8 +11,25 @@ public partial class ClimaActual : ContentPage
 		InitializeComponent();
 	}
 
-    private void OnConsultarClicked(object sender, EventArgs e)
+    private async void OnConsultarClicked(object sender, EventArgs e)
     {
+		string latitud = lat.Text;
+		string longitud = lon.Text;
 
+		if (Connectivity.NetworkAccess != NetworkAccess.Internet) 
+		{
+			return;
+		}
+		using(var httpclient = new HttpClient()) 
+		{
+			string url = "https://api.openweathermap.org/data/2.5/weather?lat="+latitud+"&lon="+longitud+ "&appid=dbc149b6f7cf80e507e48c24f912c3ac";
+			var response = await httpclient.GetAsync(url);
+			if (response.IsSuccessStatusCode) 
+			{
+				var json = await response.Content.ReadAsStringAsync();
+				var clima = JsonConvert.DeserializeObject<Rootobject>(json);
+				weatherLabel.Text = clima.weather[0].main;
+			}
+		}
     }
 }
